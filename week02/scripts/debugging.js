92b22131ed65edb93e6f66edb7d0f6c10f7e5d60
Francisco Moroni Lopez Guzman <moroni@192.168.1.74>
2025-07-11 18:31:15 -0600

    feat: add HTML and JavaScript files for JavaScript debugging example

---
+++ week02/scripts/debugging.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const radiusOutput = document.getElementById("radius");
+    const areaOutput = document.getElementById("area");
+
+    let area = 0;
+    const PI = 3.14159;
+
+    let radius = 10;
+    area = PI * radius * radius;
+    radiusOutput.textContent = radius;
+    areaOutput.textContent = area;
+});
