e96740044e0b2b414421eef9507993518312da58
Francisco Moroni Lopez Guzman <moroni@192.168.1.3>
2025-07-12 01:42:33 -0600

    feat: implement responsive navigation and layout for temple album

---
+++ scripts/temples.js
@@ -0,0 +1,9 @@
+const mainnav = document.querySelector(".navigation");
+const nav = document.querySelector("nav");
+const hambutton = document.querySelector("#menu");
+
+hambutton.addEventListener("click", () => {
+    nav.classList.toggle("show");
+    navigation.classList.toggle("show");
+    menuButton.classList.toggle("show");
+});
